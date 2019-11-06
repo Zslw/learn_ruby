c91af791143c1dd8216f161772a902129ee6a66a
Zslw <dan.juarez192000@gmail.com>
2019-11-05 21:12:24 -0600

    finished excersice 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
   #write your code here
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    seconds = @seconds
+    @seconds = padded(seconds/3600) + ':' + padded(seconds%3600/60) + ':' + padded(seconds%60)
+  end
+
+  def padded(num)
+
+    if num < 10
+      return '0' + num.to_s
+    else
+      return num.to_s
+    end
+  end
 end
